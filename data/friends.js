0c21bfaef10c8abdcaa4ba46df5dc197bd2adae9
Developer <developer@Creatives-MacBook-Pro.local>
2018-04-27 22:22:55 -0700

    working code

--- data/friends.js
+++ data/friends.js
@@ -5,16 +5,9 @@
 let friendsData = [
 
     {
-        "name": "Bob",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5
-
-        ]
+        name: "backend spiderman",
+        photo: "https://i.ytimg.com/vi/K4zm30yeHHE/maxresdefault.jpg",
+        scores: [4, 1, 2, 4, 4]
     }
 
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,10 +28,86 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
 
-        friendsData.push(req.body);
-        res.json(friendsData);
+
+        currentUser = req.body
+        friendsData.push(currentUser);
+
+        //sort this array in descending order
+        //map all users to this array with reduced score
+
+        let userMatch;
+
+
+
+        compareUsers();
+        //loop through all scores and compare them with the current user array
+        function compareUsers() {
+
+
+
+
+            let usersWithReducedScores = []
+
+
+            for (let i = 0; i < friendsData.length; i++) {
+
+
+
+                //Map out a new user object whose score value is an array of difference between the two users
+                let comparedScores = currentUser.scores.map(element => Math.abs(element - friendsData[i].scores[i]));
+                let reducer = (accumulator, currentValue) => accumulator + currentValue;
+                totalDifference = comparedScores.reduce(reducer);
+
+                totalDifferenceUser = {
+
+                    name: friendsData[i].name,
+                    photo: friendsData[i].photo,
+                    totalDifference: totalDifference
+
+                }
+
+                usersWithReducedScores.push(totalDifferenceUser);
+            }
+
+
+            let descendingTotalDifference = function(a, b) { return a.totalDifference - b.totalDifference }
+            usersWithReducedScores.sort(descendingTotalDifference);
+
+            userMatch = usersWithReducedScores[0]
+
+
+        }
+
+
+        console.log(friendsData);
+        res.json(userMatch);
+
 
     });
+
+
+
+    //Now resuce the array to a single number
+
+    //push the object to the BIG array, and sort the BIG array by this value
+
+
+
+    //REDUCE
+
+
+    //SORT
+
+
+
+    //Compare the current indexes score with the current user
+
+    //reduce to a single value
+
+    //push the single value to the all scores array
+
+
+
 
 
     app.post("/api/clear", function() {
